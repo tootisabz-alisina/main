728b736b419049dbeda7e2b9ee42f04c38567cef
Alisina <a.haidari@tootisabz.tech>
2025-03-09 09:26:13 +0430

    new changes

--- ContinueLearing/HapticBootcamp.swift
+++ ContinueLearing/HapticBootcamp.swift
@@ -1,18 +1,35 @@
-//
-//  HapticBootcamp.swift
-//  ContinueLearing
-//
-//  Created by TOTI SABZ on 3/3/25.
-//
 
 import SwiftUI
 
-struct HapticBootcamp: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class HapticManager {
+    
+    static let instance = HapticManager() // Singleton
+    
+    func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
     }
+    
+    func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.impactOccurred()
+    }
+    
 }
 
-#Preview {
-    HapticBootcamp()
+struct HapticsBootcamp: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            
+            Button("success") { HapticManager.instance.notification(type: .success) }
+            Button("warning") { HapticManager.instance.notification(type: .warning) }
+            Button("error") { HapticManager.instance.notification(type: .error) }
+            Divider()
+            Button("soft") { HapticManager.instance.impact(style: .soft) }
+            Button("light") { HapticManager.instance.impact(style: .light) }
+            Button("medium") { HapticManager.instance.impact(style: .medium) }
+            Button("rigid") { HapticManager.instance.impact(style: .rigid) }
+            Button("heavy") { HapticManager.instance.impact(style: .heavy) }
+        }
+    }
 }
